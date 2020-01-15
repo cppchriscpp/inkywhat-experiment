5b1d079df563a09adeb19d2da936838828584454
cppchriscpp <admin@cpprograms.net>
2020-01-14 22:28:08 -0500

    First pass

---
+++ main.py
@@ -0,0 +1,29 @@
+from inky import InkyWHAT as Ink
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import socket
+
+inkR = Ink('red')
+
+# im = Image.new('P', (inkR.WIDTH, inkR.HEIGHT))
+im = Image.open('/home/pi/layout.png')
+draw = ImageDraw.Draw(im)
+
+now = datetime.now()
+prettyTime = now.strftime('%a, %b %d %I:%M %p')
+font = ImageFont.load_default()
+draw.text((262, 283), prettyTime, inkR.BLACK, font)
+
+myIp = 'Unknown'
+try:
+    hostname = socket.gethostname()
+    myIp = socket.gethostbyname(hostname)
+except:
+    myIp = 'Unknown'
+
+draw.text((10, 283), myIp, inkR.BLACK, font)
+
+
+
+inkR.set_image(im)
+inkR.show()
