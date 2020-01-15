c08aa6d417af866931f67134af16d76ecee0a138
cppchriscpp <admin@cpprograms.net>
2020-01-14 23:21:19 -0500

    Attempt at feed parsing

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from inky import InkyWHAT as Ink
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 import socket
+import feedparser
+
+# TODO: Make a config file, or use environment config, or something..
+feedUrl = 'http://rss.slashdot.org/Slashdot/slashdotMain'
+
+rssData = feedparser.parse(feedUrl)
 
 inkR = Ink('red')
 
@@ -25,7 +31,9 @@ except:
 
 draw.text((10, 283), myIp, inkR.BLACK, font)
 
+# Origin (208, 12) for right column
 
+draw.text((208, 12), rssData['feed']['title'], inkR.BLACK, font)
 
 inkR.set_image(im)
 inkR.show()
