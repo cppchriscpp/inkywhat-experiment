d119642ce03aa2348db879b3b88a00e04c1b0697
cppchriscpp <admin@cpprograms.net>
2020-01-26 13:42:43 -0500

    I'm looking at the code itself... why didn't that work?

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ draw.text((12 + left, 12), wtitle, inkR.BLACK, font)
 
 draw.text((12, 32), "Currently: " + forecast.daily.summary, inkR.BLACK, font)
 draw.text((12, 44), 'Temperature: ' +  str(forecast.currently.temperature) + '°f', inkR.BLACK, font)
-draw.text((12, 56), 'High: ' + str(forecast.daily.temperature_high) + '°f  Low: ' + str(forecast.daily.temperature_low) + '°f', inkR.BLACK, font)
+draw.text((12, 56), 'High: ' + str(forecast.daily.temperatureHigh) + '°f  Low: ' + str(forecast.daily.temperatureLow) + '°f', inkR.BLACK, font)
 if len(forecast.alerts) > 0:
     draw.text((12, 68), forecast.alerts[0].title, inkR.RED, font)
 
