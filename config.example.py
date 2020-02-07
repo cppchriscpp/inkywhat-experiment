98800975135c5c7512fa3ec0f39e6f93d5de5ae5
cppchriscpp <admin@cpprograms.net>
2020-02-06 22:23:08 -0500

    dummy name

--- config.example.py
+++ config.example.py
@@ -2,3 +2,4 @@ darkskyApiKey = 'example key'
 weatherLocation = "Boston, MA"
 rssFeedUrl = 'http://rss.slashdot.org/Slashdot/slashdotMain'
 rssFeed2Url = 'http://rss.slashdot.org/Slashdot/slashdotMain'
+rssFeed2Name = 'cpprograms'
